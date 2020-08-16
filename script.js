f4e1a6935822ec00fb998bf3cb06e9e0cb7323f4
Pauline-V <67329434+Pauline-V@users.noreply.github.com>
2020-08-16 20:45:05 +0100

    Added search function for city and current location

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-//Current Weather
-
-function currentWeather(response) {
-  let currentTemperature = document.querySelector("#currentTemp");
-  currentTemperature.innerHTML = Math.round(response.data.main.temp);
-  let currentCity = document.querySelector(".currentCity");
-  currentCity.innerHTML = response.data.name;
-  let feelsLike = document.querySelector("#feelsLike");
-  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = Math.round(response.data.main.humidity);
-  let wind = document.querySelector("#wind");
-  wind.innerHTML = Math.round(response.data.wind.speed);
-  let description = document.querySelector(".description");
-  description.innerHTML = response.data.weather[0].description;
-}
-
-let apiKey = "91de7449ab8633be763a2c086e4ca924";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-
-axios.get(apiURL).then(currentWeather);
-
 //Current Date
 
 function today(data) {
@@ -66,3 +44,58 @@ function today(data) {
 
 let currentDate = document.querySelector(".currentDate");
 currentDate.innerHTML = today();
+
+//Current Weather
+
+function currentWeather(response) {
+  let currentTemperature = document.querySelector("#currentTemp");
+  let currentCity = document.querySelector(".currentCity");
+  let feelsLike = document.querySelector("#feelsLike");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let description = document.querySelector(".description");
+  let currentEmoji = document.querySelector("#currentEmoji");
+
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+  currentCity.innerHTML = response.data.name;
+  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  humidity.innerHTML = Math.round(response.data.main.humidity);
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  description.innerHTML = response.data.weather[0].description;
+  currentEmoji.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
+
+function searchCity(city) {
+  let apiKey = `91de7449ab8633be763a2c086e4ca924`;
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
+  axios.get(apiURL).then(currentWeather);
+}
+
+function submit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#inputCity");
+  searchCity(cityInput.value);
+}
+
+searchCity("London");
+
+let form = document.querySelector("#searchCity");
+form.addEventListener("submit", submit);
+
+//current Position button
+function currentPosition(position) {
+  let apiKey = "91de7449ab8633be763a2c086e4ca924";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(currentWeather);
+}
+
+function getLoc() {
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+
+let button = document.querySelector("#changetoCurrentLoc");
+button.addEventListener("click", getLoc);
